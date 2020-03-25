b89426f832ebfd36564527b3c4e9ff87f4b18c74
Petterson <58094725+hahuma@users.noreply.github.com>
2020-03-24 22:55:10 -0300

    código

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-//I could have made some simplier functions, but I'm just focused on it works for now
-
 const canvas = document.getElementById('calc')
 const ctx = canvas.getContext('2d')
 var inputValues = "";
+ctx.font = "30px Arial"
 
-function getNumberValue(numberValue) { //this function get the value and put that into canvas using the method fillText()
-  ctx.clearRect(0, 0, 283, 60) // I added that line because of some canvas problems
-  inputValues += numberValue
-  ctx.font = "30px Arial"
+
+function updatingScreen(){
+  ctx.clearRect(0, 0, 283, 60)
   ctx.fillText(inputValues, 0, 30, 283)
 }
-
-function getOperationsValue(operationsValue){
-  checkImpossibilities()
-  ctx.clearRect(0, 0, 283, 60) // I also added that because of some problems with canvas
-  inputValues += ` ${operationsValue} `
-  ctx.fillText(inputValues, 0, 30, 283)
-}
-
 function clearCalc() {
-  console.log("It's working!")
   inputValues = ""
   ctx.clearRect(0, 0, 283, 60)
 }
@@ -27,17 +16,29 @@ function clearCalc() {
 function showResult() {
   var result = eval(inputValues)
   inputValues = result
-  ctx.clearRect(0, 0, 283, 60)
-  ctx.fillText(result, 0, 30, 283)
+  updatingScreen()
 }
 
-// Impossible Cases
 
-function checkImpossibilities(){
-  let checkingString = inputValues.substring(inputValues.lenght -1, inputValues.lenght)
-  if (checkingString == "+" || checkingString == "-" || checkingString == "*" || checkingString == "/"){
-    console.log("essa função foi chamada");
-  }else{
-    return console.log(false)
+
+function allInputs(buttonValue) {
+  const allButtonsValues = {
+    operators: ["+","-","*","/"],
+    numbers:["1","2","3","4","5","6","7","8","9","0"],
+    point: "."
+  };
+  var anomalyOnString = inputValues.charAt(inputValues.length -2)
+  if (allButtonsValues.numbers.indexOf(buttonValue) != -1 ){
+    inputValues += buttonValue
+  }else if(
+    inputValues.valueOf() == "" && allButtonsValues.point.indexOf(buttonValue) != -1){
+      inputValues = "0"
+      inputValues += buttonValue
+    }else if(
+    allButtonsValues.operators.indexOf(buttonValue) != -1
+    && anomalyOnString.indexOf(buttonValue) == -1
+    && allButtonsValues.operators.indexOf(anomalyOnString) == -1){
+      inputValues += ` ${buttonValue} `
   }
+  updatingScreen()
 }
